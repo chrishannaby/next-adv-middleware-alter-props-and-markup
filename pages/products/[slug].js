ceb6915b226372d68079ea2a908d54072d4f7eca
Chris Hannaby <chris.hannaby@netlify.com>
2022-11-07 12:00:55 -0800

    add dynamic page

---
+++ pages/products/[slug].js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+const Page = ({ prices, unit, slug }) => {
+	const [test, setTest] = useState(false);
+	return (
+		<div>
+			<h1 id="message">SSG page with middleware: {slug}</h1>
+			<p>
+				Price:{' '}
+				<span
+					id="price"
+					data-prices={encodeURIComponent(JSON.stringify(prices))}
+				>
+					{prices[unit]}
+				</span>
+			</p>
+			<button onClick={() => setTest(!test)}>Click me</button>
+			<p>{test}</p>
+		</div>
+	);
+};
+
+export async function getStaticProps({ params }) {
+	const { slug } = params;
+	return {
+		props: {
+			slug,
+			prices: {
+				yard: 3,
+				foot: 1,
+			},
+			unit: 'yard',
+		},
+	};
+}
+
+export default Page;
